efe0bcf7b5dcd8739c447a500419f76c6ed842f0
Erik Taheri <etaheri@me.com>
2022-11-08 20:54:39 -0500

    send text messag

--- netlify/functions/carnoustie.js
+++ netlify/functions/carnoustie.js
@@ -1,5 +1,7 @@
 import fetch from "node-fetch";
 import FormData from "form-data";
+import { XMLParser } from "fast-xml-parser";
+const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 export const handler = async (event, context) => {
   var formdata = new FormData();
@@ -14,12 +16,24 @@ export const handler = async (event, context) => {
   };
 
   const response = await fetch(
-    "https://www.carnoustiegolflinks.com/OnlineBooking3/php/ajax/Diary_SelectTimesByCriteria.xml.php"
+    "https://www.carnoustiegolflinks.com/OnlineBooking3/php/ajax/Diary_SelectTimesByCriteria.xml.php",
+    requestOptions
   );
   const data = await response.text();
 
+  const parser = new XMLParser();
+  let jsonObj = parser.parse(data);
+
+  client.messages
+  .create({
+     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+     from: '+12183046412',
+     to: '+17163596715'
+   })
+  .then(message => console.log(message.sid));
+
   return {
     statusCode: 200,
-    body: data,
+    body: JSON.stringify(jsonObj),
   };
 };
